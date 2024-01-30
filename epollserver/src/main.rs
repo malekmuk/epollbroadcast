0add41c3ad051c171e457d007bd8ae4f16fd11e6
mmukah2 <mmukah2@uic.edu>
2024-01-29 23:07:56 -0600

    add active client map, simplify broadcast_message(), and revise error handling

--- epollserver/src/main.rs
+++ epollserver/src/main.rs
@@ -1,5 +1,6 @@
+use std::collections::HashSet;
 use std::net::TcpListener;
-use std::io::{Error, ErrorKind, Result};
+use std::io::{Error, Result};
 use std::os::fd::AsRawFd;
 use structopt::StructOpt;
 
@@ -27,24 +28,22 @@ impl ClientState {
     }
 }
 
-fn broadcast_message(cfd: i32, clients: &Vec<ClientState>) {
-    let client = clients.get(cfd as usize).unwrap();
-    let message = client.buf.as_ptr() as *const libc::c_void;
-    let len = client.buf.len();
+fn broadcast_message(orator: &ClientState, active: &HashSet<i32>) {
+    let message = orator.buf.as_ptr() as *const libc::c_void;
+    let len = orator.buf.len();
 
-    for client in clients {
-        if client.fd >= 0 && client.fd != cfd {
-            unsafe {libc::write(client.fd, message, len)};
+    for client in active.iter() {
+        if *client != orator.fd {
+            unsafe { libc::write(*client, message, len) };
         }
     }
 }
 
-fn handle_client(epfd: i32, cfd: i32, clients: &mut Vec<ClientState>) {
+fn handle_client(epfd: i32, cfd: i32, clients: &mut Vec<ClientState>, active: &mut HashSet<i32>) {
     let client = clients.get_mut(cfd as usize).unwrap();
     let bufptr = unsafe { client.buf.as_mut_ptr().offset(client.buf.len() as isize) as *mut libc::c_void };
     let count = client.buf.capacity() - client.buf.len();
     let ret = unsafe { libc::read(cfd, bufptr, count) };
-    unsafe { client.buf.set_len(client.buf.len() + ret as usize) };
 
     match ret {
         -1 => {
@@ -53,39 +52,37 @@ fn handle_client(epfd: i32, cfd: i32, clients: &mut Vec<ClientState>) {
                 match errno {
                     libc::EAGAIN => { return; },
                     _ => {
-                        eprintln!("unexpected read error: {} -- client (fd = {}) being removed!", errno, cfd);
-                        remove_client(epfd, cfd, clients);
+                        eprintln!("read error (removing fd = {}): {}", cfd, Error::last_os_error());
+                        remove_client(epfd, cfd, active);
                     }
                 }
             }
         }
         0 => {
-            remove_client(epfd, cfd, clients);
+            remove_client(epfd, cfd, active);
         }
         _ => {
-            if client.buf.ends_with(b"\n") || client.buf.len() == BUFFER_SIZE {
-                broadcast_message(cfd, clients);
+            unsafe { client.buf.set_len(client.buf.len() + ret as usize) };
 
-                /* Need to get new mutable reference after broadcast_message() */
-                clients.get_mut(cfd as usize).unwrap().buf.clear();
+            if client.buf.ends_with(b"\n") || client.buf.len() == BUFFER_SIZE {
+                broadcast_message(client, active);
+                client.buf.clear();
             } 
         }
     }
 }
 
-fn remove_client(epfd: i32, cfd: i32, clients: &mut Vec<ClientState>) {
+fn remove_client(epfd: i32, cfd: i32, active: &mut HashSet<i32>) {
     unsafe { 
         libc::epoll_ctl(epfd, libc::EPOLL_CTL_DEL, cfd, std::ptr::null_mut()); 
         libc::close(cfd);
     }
 
-    if let Some(client) = clients.get_mut(cfd as usize) {
-        client.fd = -1;
-        println!("removed client {}", cfd);
-    }
+    active.remove(&cfd);
+    println!("removed client {}", cfd);
 }
 
-fn accept_client(epfd: i32, sockfd: i32, clients: &mut Vec<ClientState>) {
+fn accept_client(epfd: i32, sockfd: i32, clients: &mut Vec<ClientState>) -> Result<i32> {
     let cfd = unsafe { 
         libc::accept4(
             sockfd, 
@@ -96,7 +93,7 @@ fn accept_client(epfd: i32, sockfd: i32, clients: &mut Vec<ClientState>) {
     };
     if cfd < 0 {
         eprintln!("failed to accept client (fd = {})", cfd);
-        return;
+        return Err(Error::last_os_error());
     }
 
     println!("accepted a client (fd = {})", cfd);
@@ -110,17 +107,19 @@ fn accept_client(epfd: i32, sockfd: i32, clients: &mut Vec<ClientState>) {
     if ret < 0 {
         eprintln!("failed to add client to epoll");
         unsafe { libc::close(cfd) };
-        return;
+        return Err(Error::last_os_error());
     }
 
-    if let Some(client) = clients.get_mut(cfd as usize) {
-        client.fd = cfd;
-        client.buf.clear();
-    }
+    let client = clients.get_mut(cfd as usize).unwrap();
+    client.fd = cfd;
+    client.buf.clear();
+
+    Ok(cfd)
 }
 
 fn await_clients(sockfd: i32, epfd: i32, events: *mut libc::epoll_event) {
     let mut clients: Vec<ClientState> = Vec::with_capacity(MAX_EVENTS as usize);
+    let mut active: HashSet<i32> = HashSet::new();
     for _i in 0..MAX_EVENTS {
         clients.push(ClientState::with_fd(-1));
     }
@@ -128,16 +127,18 @@ fn await_clients(sockfd: i32, epfd: i32, events: *mut libc::epoll_event) {
     loop {
         let ready = unsafe { libc::epoll_wait(epfd, events, MAX_EVENTS, -1) };
         if ready < 0 {
-            unsafe { eprintln!("epoll_wait error: {}", *(libc::__errno_location())); }
+            eprintln!("epoll_wait error: {}", Error::last_os_error());
             continue;
         }
 
         for i in 0..ready as isize {
             if let Some(event) = unsafe { events.offset(i).as_ref() } {
                 if event.u64 == sockfd as u64 {
-                    accept_client(epfd, sockfd, &mut clients);
+                    if let Ok(cfd) = accept_client(epfd, sockfd, &mut clients) {
+                        active.insert(cfd);
+                    }
                 } else {
-                    handle_client(epfd, event.u64 as i32, &mut clients);
+                    handle_client(epfd, event.u64 as i32, &mut clients, &mut active);
                 }
             }
         }
@@ -157,14 +158,14 @@ fn epoll_init(sockfd: i32) -> Result<i32> {
             if libc::epoll_ctl(epfd, libc::EPOLL_CTL_ADD, sockfd, &mut e) == 0 {
                 return Ok(epfd);
             } else {
-                let errmsg = format!("epoll_ctl failed to add server fd {} -- OS Error: {}", sockfd, *libc::__errno_location());
-                return Err(Error::new(ErrorKind::Other, errmsg));
+                let errmsg = format!("epoll_ctl failed to add server fd {} -- {}", sockfd, Error::last_os_error());
+                return Err(Error::other(errmsg));
             }
         }
-
-        let errmsg = format!("epoll_create1 failed -- OS Error: {}", *libc::__errno_location());
-        Err(Error::new(ErrorKind::Other, errmsg))
     }
+    
+    let errmsg = format!("epoll_create1 failed -- {}", Error::last_os_error());
+    Err(Error::other(errmsg))
 }
 
 fn main() -> Result<()> {
